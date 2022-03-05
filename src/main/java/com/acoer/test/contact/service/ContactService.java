6bbeee542c2d15e93931157b6077d7db0db8faa5
shiftALpha <ranadhirav08@gmail.com>
2022-03-05 21:44:19 +0200

    To fix & complete - contactService

--- src/main/java/com/acoer/test/contact/service/ContactService.java
+++ src/main/java/com/acoer/test/contact/service/ContactService.java
@@ -12,42 +12,61 @@ import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.stereotype.Service;
 
 import com.acoer.test.contact.domain.Contact;
+import com.acoer.test.contact.repo.contactRepo;
+
+import com.acoer.test.contact.controller.*;
 
 @Service
 public class ContactService implements ICrudOperations<Contact> {
 
 	private static final Logger logger = LogManager.getLogger();
-	
+
 	@Autowired
 	private MongoOperations mongoOps;
 
 	@Override
 	public List<Contact> getAll() {
-		// TODO Add the implementation code
+		// return contactRepo.getAll();
 		return null;
 	}
 
 	@Override
 	public List<Contact> search(String searchTerm) {
-		// TODO Add the implementation code
+
+		return null;
+		// return contactRepo.search(searchTerm).orElseThrow(() -> new ResourceNotFoundException(UNABLE_TO_LOCATE));
+	}
+
+	@Override
+	public Contact add(Contact itemContact) {
+
+		// return contactRepo.save(item);
 		return null;
 	}
 
 	@Override
-	public Contact add(Contact item) {
-		// TODO Add the implementation code
-		return null;
-	}
+	public Contact update(Contact newNumber) {
+		
+		// if(number == null) new com.acoer.test.contact.controller.IllegalArgumentException("");
 
-	@Override
-	public Contact update(Contact item) {
-		// TODO Add the implementation code
-		return null;
+		// 	return contactRepo.search(searchTerm).map(contact -> {
+		// 		Contact.setNum(newNumber.getNum());
+				
+
+
+		// 	}).orElseGet(() -> 
+		// 	{
+		// 		new ResourceNotFoundException("Unable to find number :" + number);
+		// 		return null;
+		// 	});
+		
+			return null;
+
 	}
 
 	@Override
 	public void delete(Contact item) {
-		// TODO Add the implementation code
+		// contactRepo.delete(item);
 
 	}
 
