dcaf9da070afbba02aa5e59239a2b43dd6b28541
shiftALpha <ranadhirav08@gmail.com>
2022-03-05 21:41:36 +0200

    Swagger2 annotation

--- src/main/java/com/acoer/test/contact/Application.java
+++ src/main/java/com/acoer/test/contact/Application.java
@@ -2,9 +2,16 @@ package com.acoer.test.contact;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
+@ComponentScan
 public class Application {
+
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
