510811381aab4579f973914b5a41a7f9f0b689a2
shiftALpha <ranadhirav08@gmail.com>
2022-03-05 21:43:22 +0200

    contact class update

--- src/main/java/com/acoer/test/contact/domain/Contact.java
+++ src/main/java/com/acoer/test/contact/domain/Contact.java
@@ -1,11 +1,44 @@
 package com.acoer.test.contact.domain;
 
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "contacts")
 public class Contact {
 
-	/**
-	 * TODO: Add the information to be stored by the contacts
-	 */
+	@Id
+	private String fName;
+	private String sName;
+	private int phoneNum;
+
+	public Contact(){
+	}
+
+	public Contact(String fName, String sName, int phoneNum){
+		this.fName = fName;
+		this.sName = sName;
+		this.phoneNum = phoneNum;
+	}
+
+	
+	public String getFName(){
+		return fName;
+	}
+	public void setFName(String fName){
+		this.fName = fName;
+	}
+
+	public String getSName(){
+		return sName;
+	}
+	public void setSName(String sName){
+		this.sName = sName;
+	}
+	
+	public int getNum(){
+		return phoneNum;
+	}
+	public void setNum(int phoneNum){
+		this.phoneNum = phoneNum;
+	}
 }
