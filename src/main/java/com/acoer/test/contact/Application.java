970d0e7dc163d920dd02a74efc65ff0f1663c9e3
shiftALpha <ranadhirav08@gmail.com>
2022-03-06 20:08:25 +0200

    Mongo Validation

--- src/main/java/com/acoer/test/contact/Application.java
+++ src/main/java/com/acoer/test/contact/Application.java
@@ -2,8 +2,10 @@ package com.acoer.test.contact;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-
+import org.springframework.data.mongodb.core.mapping.event.ValidatingMongoEventListener;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
@@ -12,6 +14,16 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class Application {
 
 	
+	@Bean
+    public ValidatingMongoEventListener validatingMongoEventListener() {
+        // return ValidatingMongoEventListener(validator());
+		return null;
+    }
+
+	@Bean
+    public LocalValidatorFactoryBean validator() {
+        return new LocalValidatorFactoryBean();
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
