f5e353d51ad4dedfea7085eb63c26e1fdbac3507
shiftALpha <ranadhirav08@gmail.com>
2022-03-06 20:10:45 +0200

    Contact Repository class(if needed)

---
+++ src/main/java/com/acoer/test/contact/repo/contactRepository.java
@@ -0,0 +1,11 @@
+package com.acoer.test.contact.repo;
+
+import com.acoer.test.contact.domain.Contact;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface contactRepository extends MongoRepository<Contact, String>{
+    
+}
