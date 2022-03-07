5f521f0ada0500da96fc12769d6b173a52ac8fe3
shiftALpha <ranadhirav08@gmail.com>
2022-03-07 15:21:36 +0200

    First Name, last Name search - Final commit

--- src/main/java/com/acoer/test/contact/repo/contactRepository.java
+++ src/main/java/com/acoer/test/contact/repo/contactRepository.java
@@ -1,5 +1,7 @@
 package com.acoer.test.contact.repo;
 
+import java.util.List;
+
 import com.acoer.test.contact.domain.Contact;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -7,4 +9,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface contactRepository extends MongoRepository<Contact, String>{
+
+    List<Contact> findByLastname(String sName);
+    List<Contact> findByFname(String fName);
 }
