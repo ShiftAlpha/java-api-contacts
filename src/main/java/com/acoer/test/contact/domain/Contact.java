645aa04385a1c59edfac40f3e08d210f723a1465
shiftALpha <ranadhirav08@gmail.com>
2022-03-07 12:26:54 +0200

    ID & Database sequence

--- src/main/java/com/acoer/test/contact/domain/Contact.java
+++ src/main/java/com/acoer/test/contact/domain/Contact.java
@@ -1,15 +1,24 @@
 package com.acoer.test.contact.domain;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Transient;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.lang.NonNull;
 
 import io.swagger.annotations.ApiModelProperty;
 
 @Document(collection = "contacts")
 public class Contact {
 
+	@Transient
+    public static final String SEQUENCE_NAME = "users_sequence";
+	
+	@Id
+	private long id;
+
 	@Id
 	@ApiModelProperty(notes = "The contacts first name")
+	@NonNull()
 	private String fName;
 	
 	@ApiModelProperty(notes = "The contacts surname")
@@ -27,6 +36,13 @@ public class Contact {
 		this.phoneNum = phoneNum;
 	}
 
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	
 	
 	public String getFName(){
 		return fName;
